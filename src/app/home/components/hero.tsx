0842311626df4fa49c0cee2564d3c9cd4caf2e8d
Gustavo <gustavods2003@outlook.com>
2025-03-11 16:46:06 -0300

    feat: iniciar página principal

---
+++ src/app/home/components/hero.tsx
@@ -0,0 +1,8 @@
+export function Hero() {
+  return (
+    <div className="bg-[url('/hero.png')] bg-cover bg-center h-[40rem]">
+      <h1 className="text-white90">Aprenda algo novo todo dia.</h1>
+      <h2>Torne-se um profissional e esteja pronto para ingressar no mundo.</h2>
+    </div>
+  );
+}
