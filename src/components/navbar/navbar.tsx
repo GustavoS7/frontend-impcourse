8921f44aba9ce3ebbce71688d544909fc9890f4e
Gustavo <gustavods2003@outlook.com>
2025-03-11 17:53:26 -0300

    feat: criar componente de navbar com variantes

---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,41 @@
+import Link from 'next/link';
+
+type TNavbar = {
+  variant?: 'primary' | 'secondary';
+};
+
+export function Navbar({ variant = 'primary' }: TNavbar) {
+  const classname = {
+    container: '',
+    title: '',
+  };
+  if (variant === 'primary') {
+    classname.container =
+      'absolute top-0 bg-none z-20 flex justify-between px-12 w-full h-20 items-center';
+    classname.title = 'text-white90 font-bold text-2xl';
+  } else if (variant === 'secondary') {
+    classname.container =
+      'flex justify-between px-12 w-full h-20 items-center border border-black30';
+    classname.title = 'text-primary font-bold text-2xl';
+  }
+
+  return (
+    <nav className={classname.container}>
+      <div>
+        <h1 className={classname.title}>ImpCourse</h1>
+      </div>
+      <div className="flex gap-10">
+        <Link href="/sign-up">
+          <p className="bg-primary p-1 text-lg text-white90 rounded-sm w-32 flex justify-center font-bold">
+            Cadastrar
+          </p>
+        </Link>
+        <button>
+          <p className="p-1 text-lg text-primary border border-primary rounded-sm w-32 flex justify-center font-bold">
+            Entrar
+          </p>
+        </button>
+      </div>
+    </nav>
+  );
+}
