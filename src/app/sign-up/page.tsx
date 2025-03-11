3c3aa51dcf2846a64dae372840811a87ce4f3aac
Gustavo <gustavods2003@outlook.com>
2025-03-11 18:00:23 -0300

    feat: iniciar form cadastro

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -5,11 +5,45 @@ export default function SignUp() {
     <div>
       <Navbar variant="secondary" />
 
-      <div>
-        <img
-          src="/analyze-data.png"
-          alt=""
-        />
+      <div className="flex gap-10 justify-center">
+        <div>
+          <img
+            src="/analyze-data.png"
+            alt=""
+          />
+        </div>
+
+        <div>
+          <form action="">
+            <div className="flex flex-col gap-1">
+              <label htmlFor="">Nome</label>
+              <input
+                type="text"
+                className="w-full bg-black30"
+              />
+            </div>
+
+            <div className="flex flex-col gap-1">
+              <label htmlFor="">E-mail</label>
+              <input
+                type="text"
+                className="w-full bg-black30"
+              />
+            </div>
+
+            <div className="flex flex-col gap-1">
+              <label htmlFor="">Senha</label>
+              <input
+                type="text"
+                className="w-full bg-black30"
+              />
+            </div>
+
+            <button className="bg-primary w-full text-white90 text-xl">
+              Cadastrar
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
