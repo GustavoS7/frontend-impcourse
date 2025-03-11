35c0d1f04601e8f177a16f97df8bb39c3601af10
Gustavo <gustavods2003@outlook.com>
2025-03-11 17:53:44 -0300

    feat: adicionar navbar a home

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,8 +1,11 @@
+import { Navbar } from '@/components/navbar';
 import { Hero } from './components';
 
 export default function Home() {
   return (
     <div className="">
+      <Navbar />
+
       <Hero />
     </div>
   );
